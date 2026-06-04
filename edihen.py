e9bac056988981425f7a6894a6bec760c731d432
volodimirmartinisin3-ux <volodimirmartinisin3@gmail.com>
2026-06-04 11:53:44 +0300

    Add files via upload

---
+++ edihen.py
@@ -0,0 +1,30 @@
+import telebot
+from groq import Groq
+import os
+
+TELEGRAM_TOKEN = os.getenv("8232647035:AAFzwYvDtdgyP7QgFf3ChT-2Rb4J_cR04IQ")
+GROQ_KEY = os.getenv("8232647035:AAFzwYvDtdgyP7QgFf3ChT-2Rb4J_cR04IQ")
+
+bot = telebot.TeleBot(TELEGRAM_TOKEN)
+client = Groq(api_key=GROQ_KEY)
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.send_message(message.chat.id, "Привіт! Я AI-асистент. Постав будь-яке питання 🤖")
+
+@bot.message_handler(func=lambda message: True)
+def ask_ai(message):
+    bot.send_message(message.chat.id, "Думаю... ⏳")
+
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {"role": "system", "content": "Ти — асистент на ім'я Тарас. Відповідай тільки українською мовою."},
+            {"role": "user", "content": message.text}
+        ]
+    )
+
+    answer = response.choices[0].message.content
+    bot.send_message(message.chat.id, answer)
+
+bot.polling()
